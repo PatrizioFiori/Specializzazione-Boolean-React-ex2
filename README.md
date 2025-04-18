8c352adfb27568eda8543550aa203f48c9055441
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-04-18 18:08:23 +0200

    Refactor App.jsx: restructure component and add product listing functionality

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Specializzazione Boolean esercizi 2
 
 - Lavorare con useState e useReduce
+
+📌 Milestone 1: Mostrare la lista dei prodotti **Obiettivo**: Vedere un elenco leggibile di tutti i prodotti con nome e prezzo.
--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,32 @@
-import { useState, useReducer } from "react";
+// App.jsx
+
+
+import React from 'react'
+import { useState, useReducer } from 'react';
+
+const products = [
+  { name: 'Mela', price: 0.5 },
+  { name: 'Pane', price: 1.2 },
+  { name: 'Latte', price: 1.0 },
+  { name: 'Pasta', price: 0.7 },
+];
+
+const App = () => {
+
+  const [lists, setLists] = useState(products)
+  //📌 Milestone 1: Mostrare la lista dei prodotti
+  // Obiettivo: Vedere un elenco leggibile di tutti i prodotti con nome e prezzo.
+  return (
+    <div>
+      {lists.map((item, index) => (
+        <div key={index}>
+          <p><strong>{item.name}</strong>: {item.price}$</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
+
+
